f0b48ca019df40ac6e59c0d38901818a47bff4b6
buonzz <buonzz@gmail.com>
2019-03-19 19:47:14 +0800

    added basic stuffs

---
+++ PatientSystem/PatientSystem/ConnectionStringBuilder.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Data.SqlClient
+
+Public Class ConnectionStringBuilder
+    Public Function getConnectionString() As String
+
+        'An often overlooked gem, SqlConnectionStringBuilder simplifies creating a connection
+        Dim csb As SqlConnectionStringBuilder = New SqlConnectionStringBuilder()
+        csb.DataSource = ".\SQLEXPRESS"
+        csb.InitialCatalog = "PatientSystem"
+        csb.IntegratedSecurity = True
+        Return csb.ConnectionString
+
+    End Function
+End Class
--- PatientSystem/PatientSystem/frmListPatient.vb
+++ PatientSystem/PatientSystem/frmListPatient.vb
@@ -1,3 +1,14 @@
-﻿Public Class frmListPatient
+﻿Option Strict On
+Option Explicit On
 
+Imports System.Data.SqlClient
+
+Public Class frmListPatient
+
+    Dim patientDataAdapter As SqlDataAdapter = Nothing
+    Dim patientDataTable As DataTable = Nothing
+
+    Private Sub frmListPatient_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
