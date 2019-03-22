28dcee2d2cd80b4b3ed154b6af5b475aa485efd7
buonzz <buonzz@gmail.com>
2019-03-22 17:41:01 +0800

    rewrite patient repo to use oledb

--- PatientSystem/PatientSystem/PatientRepository.vb
+++ PatientSystem/PatientSystem/PatientRepository.vb
@@ -1,27 +1,30 @@
 ﻿Option Strict On
 Option Explicit On
+Imports System.Data.OleDb
 Imports System.Data.SqlClient
 
 Public Class PatientRepository
 
     Dim cn_builder As ConnectionStringBuilder
+    Dim cn As OleDb.OleDbConnection
 
     Public Sub New()
         cn_builder = New ConnectionStringBuilder
+        cn = New OleDbConnection
+        cn.ConnectionString = cn_builder.getConnectionString()
     End Sub
 
     Public Function add(name As String, sex As String, birthday As String,
                         age As Integer, weight As Double, height As Double,
                         mdate As String, txtDisease As String, txtTreatment As String) As Integer
 
-        Dim cn As SqlConnection = New SqlConnection(cn_builder.getConnectionString())
         Dim patient_added As Integer
         Dim new_patient_id As Integer = 0
 
 
         Try
             cn.Open()
-            Dim cmd As SqlCommand = New SqlCommand("insert into patients(name,sex,birthday,age,weight,height,mdate) values(@name,@sex,@birthday,@age,@weight,@height,@mdate);SELECT CAST(scope_identity() AS int)", cn)
+            Dim cmd As OleDbCommand = New OleDbCommand("insert into patients(name,sex,birthday,age,weight,height,mdate) values(@name,@sex,@birthday,@age,@weight,@height,@mdate);SELECT CAST(scope_identity() AS int)", cn)
             cmd.Parameters.AddWithValue("@name", name)
             cmd.Parameters.AddWithValue("@sex", sex)
             cmd.Parameters.AddWithValue("@birthday", birthday)
@@ -38,10 +41,10 @@ Public Class PatientRepository
 
             ' if the patient is added, then enter new entries to the checkup repo
             If patient_added = 1 Then
-                Dim checkupRepo As CheckupRepository = New CheckupRepository()
-                Dim historyRepo As HistoryRepository = New HistoryRepository()
+                ' Dim checkupRepo As CheckupRepository = New CheckupRepository()
+                ' Dim historyRepo As HistoryRepository = New HistoryRepository()
 
-                checkupRepo.add(new_patient_id, mdate)
+                ' checkupRepo.add(new_patient_id, mdate)
 
             End If
         Catch ex As Exception
@@ -57,12 +60,11 @@ Public Class PatientRepository
     Public Function edit(name As String, sex As String, birthday As String,
                         age As Integer, weight As Double, height As Double, mdate As String, id As Integer) As Integer
 
-        Dim cn As SqlConnection = New SqlConnection(cn_builder.getConnectionString())
         Dim count As Integer
 
         Try
             cn.Open()
-            Dim cmd As SqlCommand = New SqlCommand("update patients set name= @name ,sex = @sex, birthday = @birthday, age = @age, weight = @weight, height = @height, mdate = @mdate where id=@id", cn)
+            Dim cmd As OleDbCommand = New OleDbCommand("update patients set name= @name ,sex = @sex, birthday = @birthday, age = @age, weight = @weight, height = @height, mdate = @mdate where id=@id", cn)
             cmd.Parameters.AddWithValue("@name", name)
             cmd.Parameters.AddWithValue("@sex", sex)
             cmd.Parameters.AddWithValue("@birthday", birthday)
