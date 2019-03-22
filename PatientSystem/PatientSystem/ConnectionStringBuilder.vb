57888e566a9f754d5fd5e645a6240317eb020327
buonzz <buonzz@gmail.com>
2019-03-22 17:32:19 +0800

    use ms access 2003

--- PatientSystem/PatientSystem/ConnectionStringBuilder.vb
+++ PatientSystem/PatientSystem/ConnectionStringBuilder.vb
@@ -1,6 +1,6 @@
 ﻿Public Class ConnectionStringBuilder
     Public Function getConnectionString() As String
-        Dim cs As String = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=patientsystem.accdb"
+        Dim cs As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source = 'patientsystem.mdb'"
         Return cs
     End Function
 End Class
